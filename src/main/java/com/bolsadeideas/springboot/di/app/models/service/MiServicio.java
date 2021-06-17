989b955be55872dc870c7119d20f39074683771f
Andres Gomez <afgomezb20@gmail.com>
2021-06-16 22:09:33 -0500

    Se crea rama issu01

---
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
@@ -0,0 +1,14 @@
+package com.bolsadeideas.springboot.di.app.models.service;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Primary
+@Component("miServicioSimple")
+public class MiServicio implements IServicio{
+
+	@Override 
+	public String operacion() {
+		return "Ejecutando algun proceso importante simple...";
+	}
+}
